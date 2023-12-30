97efce99eb1166cfc352570bd936ef303dd50cb6
emckeown394 <125586249+emckeown394@users.noreply.github.com>
2023-12-30 17:04:53 +0000

    signup backend

--- app.js
+++ app.js
@@ -88,6 +88,64 @@ app.get("/signup", async (req,res) => {
   res.render('signup');
 });
 
+//adding signup credentials to database
+app.post('/signup', (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Regular expression to check password requirements
+  // Must contain at least one uppercase and lower case letter, one digit 
+  //and be at least 8 characters long
+  const passwordCheck = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+  // Check if the password meets the requirements
+  if (!password.match(passwordCheck)) {
+    return res.status(400).send('<code>Password must be at least 8 characters long and contain an uppercase letter, lowercase letter, and a number.</code> <a href="/signup" class="button">BACK</a>');
+
+  }
+
+  // Check if the email contains "@"
+  if (!email.includes('@')) {
+    console.log('Invalid email format:', email);
+    return res.status(400).send('<code>Invalid email format. Please provide a valid email address.</code> <a href="/signup" class="button">BACK</a>');
+  }
+
+
+  // Query to check if the users email already exists in the database
+  db.query(
+    `SELECT COUNT(*) AS count FROM students WHERE email = ?`,
+    [email],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(400).send('<code>This email already exists. Please use another email.</code> <a href="/signup" class="button">BACK</a>');
+      }
+
+      // If the email already exists, inform the student to provide a different email
+      if (results[0].count > 0) {
+        return res.status(400).send('Email already taken. Please use another email.');
+      }
+
+      // If all conditions are met, hash the password and insert user into the database
+      const hashedPassword = bcrypt.hashSync(password, 10);
+
+      console.log('Hashed Password:', hashedPassword)
+
+      db.query(
+        `INSERT INTO students (name, email, password) VALUES (?, ?, ?)`,
+        [name, email, hashedPassword],
+        (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).send('An error occurred during signup.');
+          } else {
+            res.redirect('/login');
+          }
+        }
+      );
+    }
+  );
+});
+
 //login
 app.get("/login", async (req,res) => {
   res.render('login');
