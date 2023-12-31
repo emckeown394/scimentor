5caa53bba2e985b04fc32d1f543ae6c835211f14
emckeown394 <125586249+emckeown394@users.noreply.github.com>
2023-12-31 13:03:22 +0000

    Teacher login/signup

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const cookieParser = require('cookie-parser');
 const { name } = require("ejs");
+const flash = require('express-flash');
 
 
 // Database connection
@@ -35,6 +36,9 @@ app.use(session({
     saveUninitialized: true
   }));
 app.use(bodyParser.urlencoded({ extended: true}));
+
+//used for error messages
+app.use(flash());
 
 
 //index page
@@ -83,12 +87,12 @@ app.get("/update_profile", (req,res) => {
   res.render('update_profile');
 });
 
-//signup
+//student signup
 app.get("/signup", async (req,res) => {
   res.render('signup');
 });
 
-//adding signup credentials to database
+//adding student signup credentials to database
 app.post('/signup', (req, res) => {
   const { name, email, password } = req.body;
 
@@ -99,14 +103,14 @@ app.post('/signup', (req, res) => {
 
   // Check if the password meets the requirements
   if (!password.match(passwordCheck)) {
-    return res.status(400).send('<code>Password must be at least 8 characters long and contain an uppercase letter, lowercase letter, and a number.</code> <a href="/signup" class="button">BACK</a>');
+    return res.render('signup', {error: 'Password must contain at least 1 lowercase and uppercase letter, 1 number and be at least 8 characters long. Please enter a new password'});
 
   }
 
   // Check if the email contains "@"
   if (!email.includes('@')) {
     console.log('Invalid email format:', email);
-    return res.status(400).send('<code>Invalid email format. Please provide a valid email address.</code> <a href="/signup" class="button">BACK</a>');
+    return res.render('signup', {error: 'Your email must include an @ symbol. Please enter a valid email'});
   }
 
 
@@ -117,18 +121,16 @@ app.post('/signup', (req, res) => {
     (err, results) => {
       if (err) {
         console.error(err);
-        return res.status(400).send('<code>This email already exists. Please use another email.</code> <a href="/signup" class="button">BACK</a>');
+        return res.render('signup', {error: 'This email already exists. Please enter another email'});
       }
 
       // If the email already exists, inform the student to provide a different email
       if (results[0].count > 0) {
-        return res.status(400).send('Email already taken. Please use another email.');
+        return res.render('signup', {error: 'This email already exists. Please enter another email'});
       }
 
       // If all conditions are met, hash the password and insert user into the database
       const hashedPassword = bcrypt.hashSync(password, 10);
-
-      console.log('Hashed Password:', hashedPassword)
 
       db.query(
         `INSERT INTO students (name, email, password) VALUES (?, ?, ?)`,
@@ -146,9 +148,155 @@ app.post('/signup', (req, res) => {
   );
 });
 
-//login
+// student login
 app.get("/login", async (req,res) => {
   res.render('login');
+});
+
+app.post('/login', function(req,res) {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  if (email && password) {
+    connection.query(
+      'SELECT * FROM students WHERE email = ?',
+      [email],
+      function(error, rows, fields) {
+        if (error) throw error;
+        let numrows = rows.length;
+
+        if (numrows > 0) {
+          const storedPassword = rows[0].password;
+          bcrypt.compare(password, storedPassword, function(err, result) {
+            if (err) throw err;
+
+            if (result) {
+              req.session.loggedin = true;
+              req.session.email = email;
+              req.session.student_id = rows[0].id;
+              res.redirect('/homepage');
+            }else{
+              return res.render('login', {error: 'Invalid email or password. Please try again'});
+            }
+          });
+        }else{
+          return res.render('login', {error: 'Invalid email or password. Please try again'});
+        }
+      }
+    );
+  }else{
+    res.send('Enter Email and Password');
+  }
+});
+
+//teacher signup
+app.get("/teachers_signup", async (req,res) => {
+  res.render('teachers_signup');
+});
+
+//adding teacher signup credentials to database
+app.post('/teachers_signup', (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Regular expression to check password requirements
+  // Must contain at least one uppercase and lower case letter, one digit 
+  //and be at least 8 characters long
+  const passwordCheck = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+  // Check if the password meets the requirements
+  if (!password.match(passwordCheck)) {
+    return res.render('teachers_signup', {error: 'Password must contain at least 1 lowercase and uppercase letter, 1 number and be at least 8 characters long. Please enter a new password'});
+
+  }
+
+  // Check if the email contains "@"
+  if (!email.includes('@')) {
+    console.log('Invalid email format:', email);
+    return res.render('teachers_signup', {error: 'Email must contain an @ symbol. Please use a valid format'});
+  }
+
+
+  // Query to check if the teachers email already exists in the database
+  db.query(
+    `SELECT COUNT(*) AS count FROM teachers WHERE email = ?`,
+    [email],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        req.flash('error', 'This email already exists. Please use another email');
+        return res.redirect('/teachers_signup');
+      }
+
+      // If the email already exists, inform the teacher to provide a different email
+      if (results[0].count > 0) {
+        req.flash('error', 'This email already exists. Please use another email');
+        return res.redirect('/teachers_signup');
+      }
+
+      // If all conditions are met, hash the password and insert teacher into the database
+      const hashedPassword = bcrypt.hashSync(password, 10);
+
+      db.query(
+        `INSERT INTO teachers (name, email, password) VALUES (?, ?, ?)`,
+        [name, email, hashedPassword],
+        (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).send('An error occurred during signup.');
+          } else {
+            res.redirect('/teacher_login');
+          }
+        }
+      );
+    }
+  );
+});
+
+//teacher login
+app.get("/teacher_login", async (req,res) => {
+  res.render('teacher_login');
+});
+
+app.post('/teacher_login', function(req,res) {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  if (email && password) {
+    connection.query(
+      'SELECT * FROM teachers WHERE email = ?',
+      [email],
+      function(error, rows) {
+        if (error) throw error;
+        let numrows = rows.length;
+
+        if (numrows > 0) {
+          const storedPassword = rows[0].password;
+          bcrypt.compare(password, storedPassword, function(err, result) {
+            if (err) throw err;
+
+            if (result) {
+              req.session.loggedin = true;
+              req.session.email = email;
+              req.session.student_id = rows[0].id;
+              res.redirect('/teacher_homepage');
+            }else{
+              return res.render('teacher_login', {error: 'Invalid email or password. Please try again'});
+            }
+          });
+        }else{
+          return res.render('teacher_login', {error: 'Invalid email or password. Please try again'});
+        }
+      }
+    );
+  }else{
+    res.send('Enter Email and Password');
+  }
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.clearCookie('connect.sid');
+  res.redirect('/');
 });
 
 
